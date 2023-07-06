6da3138e27da262ea671d937eea3b494eb1e6ee8
lingfengsun <lingfengsun@berkeley.edu>
2023-07-06 17:50:19 -0400

    0706 checkpoint

--- generate_robot_instruction.py
+++ generate_robot_instruction.py
@@ -23,17 +23,21 @@ import utils
 
 import fire
 
-def encode_prompt(prompt_instructions):
+def encode_prompt():
     """Encode multiple prompt instructions into a single string."""
     prompt = open("./prompt.txt").read() + "\n"
     # TODO: add seeded instructions
     # TODO: optional: add examples of subtasks
     # TODO: add functions for subskills
-    return prompt
+    # make it gpt-4 format
+    message = [
+        {"role": "user", "content": prompt}]
+    return message
 
-def post_process_gpt3_response(num_prompt_instructions, response):
+def post_process_chat_response(num_prompt_instructions, response):
     if response is None:
         return []
+    ### Parse the response into pairs of instruction, input, output ###
     raw_instructions = f"{num_prompt_instructions+1}. Instruction:" + response["text"]
     raw_instructions = re.split("###", raw_instructions)
     instructions = []
@@ -41,6 +45,7 @@ def post_process_gpt3_response(num_prompt_instructions, response):
         # if the decoding stops due to length, the last example is likely truncated so we discard it
         if idx == len(raw_instructions) - 1 and response["finish_reason"] == "length":
             continue
+        ##### Parse the response into instruction, input, output #####
         idx += num_prompt_instructions + 1
         splitted_data = re.split(f"{idx}\.\s+(Instruction|Input|Output):", inst)
         if len(splitted_data) != 7:
@@ -50,39 +55,12 @@ def post_process_gpt3_response(num_prompt_instructions, response):
             input = splitted_data[4].strip()
             input = "" if input.lower() == "<noinput>" else input
             output = splitted_data[6].strip()
+
+        ##### FILTER OUT Negative Examples #####
         # filter out too short or too long instructions
         if len(inst.split()) <= 3 or len(inst.split()) > 150:
             continue
         # filter based on keywords that are not suitable for language models.
-        blacklist = [
-            "image",
-            "images",
-            "graph",
-            "graphs",
-            "picture",
-            "pictures",
-            "file",
-            "files",
-            "map",
-            "maps",
-            "draw",
-            "plot",
-            "go to",
-            "video",
-            "audio",
-            "music",
-            "flowchart",
-            "diagram",
-        ]
-        blacklist += []
-        if any(find_word_in_string(word, inst) for word in blacklist):
-            continue
-        # We found that the model tends to add "write a program" to some existing instructions, which lead to a lot of such instructions.
-        # And it's a bit comfusing whether the model need to write a program or directly output the result.
-        # Here we filter them out.
-        # Note this is not a comprehensive filtering for all programming instructions.
-        if inst.startswith("Write a program"):
-            continue
         # filter those starting with punctuation
         if inst[0] in string.punctuation:
             continue
@@ -95,28 +73,23 @@ def post_process_gpt3_response(num_prompt_instructions, response):
 def generate_instruction_following_chat_data(
     output_dir="./gpt4_generation/",
     seed_tasks_path="./seed_tasks.jsonl",
-    num_instructions_to_generate=5,
+    num_instructions_to_generate=1,
     model_name="gpt-4",
-    num_prompt_instructions=3,
+    num_prompt_instructions=0,
     request_batch_size=1,
     temperature=1.0,
     top_p=1.0,
     num_cpus=8,
 ):
-    seed_tasks = [json.loads(l) for l in open(seed_tasks_path, "r")]
-    seed_instruction_data = [
-        {"instruction": t["instruction"], "input": t["instances"][0]["input"], "output": t["instances"][0]["output"]}
-        for t in seed_tasks
-    ]
-    print(f"Loaded {len(seed_instruction_data)} human-written seed instructions")
+    ### Load the seed tasks. Optinal. TODO ###
 
     os.makedirs(output_dir, exist_ok=True)
     request_idx = 0
-    # load the LM-generated instructions
+    ### load the LM-generated previous instructions. ###
     machine_instruction_data = []
-    if os.path.exists(os.path.join(output_dir, "regen.json")):
-        machine_instruction_data = utils.jload(os.path.join(output_dir, "regen.json"))
-        print(f"Loaded {len(machine_instruction_data)} machine-generated instructions")
+    # if os.path.exists(os.path.join(output_dir, "regen.json")):
+    #     machine_instruction_data = utils.jload(os.path.join(output_dir, "regen.json"))
+    #     print(f"Loaded {len(machine_instruction_data)} machine-generated instructions")
 
     # similarities = {}
     scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=False)
@@ -127,10 +100,10 @@ def generate_instruction_following_chat_data(
         progress_bar.update(len(machine_instruction_data))
 
     # first we tokenize all the seed instructions and generated machine instructions
-    all_instructions = [d["instruction"] for d in seed_instruction_data] + [
-        d["instruction"] for d in machine_instruction_data
-    ]
-    all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
+    # all_instructions = [d["instruction"] for d in seed_instruction_data] + [
+    #     d["instruction"] for d in machine_instruction_data
+    # ]
+    # all_instruction_tokens = [scorer._tokenizer.tokenize(inst) for inst in all_instructions]
 
     while len(machine_instruction_data) < num_instructions_to_generate:
         request_idx += 1
@@ -138,8 +111,8 @@ def generate_instruction_following_chat_data(
         batch_inputs = []
         for _ in range(request_batch_size):
             # only sampling from the seed tasks
-            prompt_instructions = random.sample(seed_instruction_data, num_prompt_instructions)
-            prompt = encode_prompt(prompt_instructions)
+            # prompt_instructions = random.sample(seed_instruction_data, num_prompt_instructions)
+            prompt = encode_prompt()
             batch_inputs.append(prompt)
         decoding_args = utils.OpenAIDecodingArguments(
             temperature=temperature,
@@ -157,41 +130,44 @@ def generate_instruction_following_chat_data(
             logit_bias={"50256": -100},  # prevent the <|endoftext|> token from being generated
         )
         request_duration = time.time() - request_start
-
-        process_start = time.time()
-        instruction_data = []
-        for result in results:
-            new_instructions = post_process_gpt3_response(num_prompt_instructions, result)
-            instruction_data += new_instructions
-
-        total = len(instruction_data)
-        keep = 0
-        for instruction_data_entry in instruction_data:
-            # computing similarity with the pre-tokenzied instructions
-            new_instruction_tokens = scorer._tokenizer.tokenize(instruction_data_entry["instruction"])
-            with Pool(num_cpus) as p:
-                rouge_scores = p.map(
-                    partial(rouge_scorer._score_lcs, new_instruction_tokens),
-                    all_instruction_tokens,
-                )
-            rouge_scores = [score.fmeasure for score in rouge_scores]
-            most_similar_instructions = {
-                all_instructions[i]: rouge_scores[i] for i in np.argsort(rouge_scores)[-10:][::-1]
-            }
-            if max(rouge_scores) > 0.7:
-                continue
-            else:
-                keep += 1
-            instruction_data_entry["most_similar_instructions"] = most_similar_instructions
-            instruction_data_entry["avg_similarity_score"] = float(np.mean(rouge_scores))
-            machine_instruction_data.append(instruction_data_entry)
-            all_instructions.append(instruction_data_entry["instruction"])
-            all_instruction_tokens.append(new_instruction_tokens)
-            progress_bar.update(1)
-        process_duration = time.time() - process_start
-        print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
-        print(f"Generated {total} instructions, kept {keep} instructions")
+        responss_text = results['choices'][0]['message']['content']
+        print(responss_text)
         utils.jdump(machine_instruction_data, os.path.join(output_dir, "regen.json"))
+
+        # process_start = time.time()
+        # instruction_data = []
+        # for result in results:
+        #     new_instructions = post_process_chat_response(num_prompt_instructions, result)
+        #     instruction_data += new_instructions
+
+        # total = len(instruction_data)
+        # keep = 0
+        # for instruction_data_entry in instruction_data:
+        #     # computing similarity with the pre-tokenzied instructions
+        #     new_instruction_tokens = scorer._tokenizer.tokenize(instruction_data_entry["instruction"])
+        #     with Pool(num_cpus) as p:
+        #         rouge_scores = p.map(
+        #             partial(rouge_scorer._score_lcs, new_instruction_tokens),
+        #             all_instruction_tokens,
+        #         )
+        #     rouge_scores = [score.fmeasure for score in rouge_scores]
+        #     most_similar_instructions = {
+        #         all_instructions[i]: rouge_scores[i] for i in np.argsort(rouge_scores)[-10:][::-1]
+        #     }
+        #     if max(rouge_scores) > 0.7:
+        #         continue
+        #     else:
+        #         keep += 1
+        #     instruction_data_entry["most_similar_instructions"] = most_similar_instructions
+        #     instruction_data_entry["avg_similarity_score"] = float(np.mean(rouge_scores))
+        #     machine_instruction_data.append(instruction_data_entry)
+        #     all_instructions.append(instruction_data_entry["instruction"])
+        #     all_instruction_tokens.append(new_instruction_tokens)
+        #     progress_bar.update(1)
+        # process_duration = time.time() - process_start
+        # print(f"Request {request_idx} took {request_duration:.2f}s, processing took {process_duration:.2f}s")
+        # print(f"Generated {total} instructions, kept {keep} instructions")
+        # utils.jdump(machine_instruction_data, os.path.join(output_dir, "regen.json"))
 
 def main(task, **kwargs):
     globals()[task](**kwargs)
